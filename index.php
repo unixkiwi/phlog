c27bf48433389e336f7f1f949071ef978b95cf73
unixkiwi <unixkiwi77@outlook.com>
2026-08-30 18:10:02 +0200

    chore: initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Phlog</title>
+</head>
+<body>
+<main>
+    <section>
+        <h1>Welcome to Phlog</h1>
+        <h3>A little blog written in purely in PHP!</h3>
+    </section>
+    <section>
+        <h2>Account</h2>
+        <form>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <input type="submit" value="Submit">
+        </form>
+    </section>
+</main>
+</body>
+</html>
