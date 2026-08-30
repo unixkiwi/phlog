f2e4b4dbcdf8a3cf508a2e01c5b9c53c7f8d7275
unixkiwi <unixkiwi77@outlook.com>
2026-08-30 19:12:23 +0200

    feat: add Database class

---
+++ classes/Database.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace classes;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static Database $instance;
+    private PDO $conn;
+
+    private string $host = 'localhost';
+    private string $dbname = 'phlog_db';
+    private string $username = 'root';
+    private string $password = '';
+
+    private function __construct()
+    {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConn()
+    {
+        return $this->conn;
+    }
+}
